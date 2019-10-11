7ab4bb8f17b1dae33c3ea3fa1576657c5cf609a4
Ariel Plomer <asplomer@gmail.com>
2019-10-11 16:07:37 -0300

    Terminado el menú

--- Arkanoid raylib/Arkanoid raylib/Arkanoid raylib.cpp
+++ Arkanoid raylib/Arkanoid raylib/Arkanoid raylib.cpp
@@ -83,11 +83,29 @@ int main(void)
 	int framesCounter = 0;
 
 	SetTargetFPS(60);
+	struct TextDivider
+	{
+		float x;
+		float y;
+		float font;
+	};
 	Rectangle Jugar;
 	Jugar.width = screenWidth / 3;
 	Jugar.height = screenHeight / 6;
 	Jugar.x = screenWidth / 2 - Jugar.width / 2;
 	Jugar.y = screenHeight / 4;
+	TextDivider playText;
+	TextDivider optionsText;
+	TextDivider exitText;
+	playText.x = 2.3f;
+	playText.y = 3.7f;
+	playText.font = 9.0f;
+	optionsText.x = 2.6f;
+	optionsText.y = 2.1f;
+	optionsText.font = 9.0f;
+	exitText.x = 2.3f;
+	exitText.y = 1.45;
+	exitText.font = 9.0f;
 	Rectangle Opciones;
 	Opciones.width = screenWidth / 3;
 	Opciones.height = screenHeight / 6;
@@ -99,10 +117,10 @@ int main(void)
 	Salir.x = screenWidth / 2 - Jugar.width / 2;
 	Salir.y = screenHeight / 1.5;
 	int opciones = 3;
-	Color colorOpciones3 = BLUE;
-	Color colorOpciones2 = WHITE;
-	Color colorOpciones1 = WHITE;
-	Color playText = WHITE;
+	Color selectedOption = WHITE;
+	Color notSelectedOption = BLUE;
+	Color selectedText = BLUE;
+	Color notSelectedText = WHITE;
 	while (!WindowShouldClose() && gamestate != closing)
 	{
 		if (gamestate == menu)
@@ -111,36 +129,37 @@ int main(void)
 			if (IsKeyPressed(KEY_UP))opciones++;
 			if (opciones < 1)opciones = 3;
 			if (opciones > 3)opciones = 1;
+			BeginDrawing();
+			ClearBackground(BLACK);
 			if (opciones == 1)
 			{
-				colorOpciones3 = WHITE;
-				colorOpciones2 = WHITE;
-				colorOpciones1 = BLUE;
-				playText = BLUE;
+				DrawRectangle(Jugar.x, Jugar.y, Jugar.width, Jugar.height, notSelectedOption);
+				DrawText("Play", screenWidth/ playText.x, screenHeight/playText.y, screenHeight/playText.font, notSelectedText);
+				DrawRectangle(Opciones.x, Opciones.y, Opciones.width, Opciones.height, notSelectedOption);
+				DrawText("Options", screenWidth / optionsText.x, screenHeight / optionsText.y, screenHeight / optionsText.font, notSelectedText);
+				DrawRectangle(Salir.x, Salir.y, Salir.width, Salir.height, selectedOption);
+				DrawText("Salir", screenWidth / exitText.x, screenHeight / exitText.y, screenHeight / exitText.font, selectedText);
 				if (IsKeyPressed(KEY_ENTER)) gamestate = closing;
 			}
 			if (opciones == 2)
 			{
-				colorOpciones3 = WHITE;
-				colorOpciones2 = BLUE;
-				colorOpciones1 = WHITE;
-				playText = BLUE;
+				DrawRectangle(Jugar.x, Jugar.y, Jugar.width, Jugar.height, notSelectedOption);
+				DrawText("Play", screenWidth/ playText.x, screenHeight/ playText.y, screenHeight/ playText.font, notSelectedText);
+				DrawRectangle(Opciones.x, Opciones.y, Opciones.width, Opciones.height, selectedOption);
+				DrawText("Options", screenWidth / optionsText.x, screenHeight / optionsText.y, screenHeight / optionsText.font, selectedText);
+				DrawRectangle(Salir.x, Salir.y, Salir.width, Salir.height, notSelectedOption);
+				DrawText("Salir", screenWidth / exitText.x, screenHeight / exitText.y, screenHeight / exitText.font, notSelectedText);
 			}
 			if (opciones == 3)
 			{
+				DrawRectangle(Jugar.x, Jugar.y, Jugar.width, Jugar.height, selectedOption);
+				DrawText("Play", screenWidth/ playText.x, screenHeight / playText.y, screenHeight/playText.font, selectedText);
+				DrawRectangle(Opciones.x, Opciones.y, Opciones.width, Opciones.height, notSelectedOption);
+				DrawText("Options", screenWidth / optionsText.x, screenHeight / optionsText.y, screenHeight / optionsText.font, notSelectedText);
+				DrawRectangle(Salir.x, Salir.y, Salir.width, Salir.height, notSelectedOption);
+				DrawText("Salir", screenWidth / exitText.x, screenHeight / exitText.y, screenHeight / exitText.font, notSelectedText);
 				if (IsKeyPressed(KEY_ENTER)) gamestate = gameplay;
-				colorOpciones3 = BLUE;
-				colorOpciones2 = WHITE;
-				colorOpciones1 = WHITE;
-				playText = WHITE;
-
 			}
-			BeginDrawing();
-			ClearBackground(BLACK);
-			DrawRectangle(Jugar.x, Jugar.y, Jugar.width, Jugar.height, colorOpciones3);
-			DrawRectangle(Opciones.x, Opciones.y, Opciones.width, Opciones.height, colorOpciones2);
-			DrawRectangle(Salir.x, Salir.y, Salir.width, Salir.height, colorOpciones1);
-			DrawText("Play", Jugar.x + Jugar.width / 3,Jugar.y+Jugar.height/6,50,playText);
 			EndDrawing();
 		}
 		if (gamestate == gameplay)
